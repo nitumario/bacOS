1736dea206cccc979f8b070ef3f15c690ec5874d
nitumario <nitu.mario@gmail.com>
2024-05-19 19:29:38 +0300

    modified:   subiecte de test/teste.json 	modified:   test.py

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 
 # C:\MinGW\bin\gcc.exe
+import subprocess
 import MySQLdb
 import sys
 import os
 import urllib.request
+import json
 
 
 db = MySQLdb.connect(host="localhost", user="mario", passwd="toor", db="bacOS")
 cursor = db.cursor()
 
 #link_file = sys.argv[1]
-#username = sys.argv[2]
+#subiect = sys.argv[2]
+#username = sys.argv[3]
+def compile_cpp(source_file):
+    compile_command = f"C:\\MinGW\\bin\\g++.exe {source_file} -o {source_file[:-4]}.exe"
+    subprocess.run(compile_command)
 
 def download(link, name):
     folder_name = name
@@ -19,5 +25,52 @@ def download(link, name):
     file_path = os.path.join(folder_name, file_name)
     urllib.request.urlretrieve(link, file_path)
 
-download('https://bashupload.com/lIhuu/1WbDK.pdf', 'mario')
+def get_tests(subiect_name):
+    query = "SELECT teste FROM subiecte WHERE nume = %s"
+    cursor.execute(query, (subiect_name,))
+    result = cursor.fetchone()
+    teste_data = json.loads(result[0][1:-1])
+    return teste_data
 
+
+
+def run_tests(teste_data, folder_name):
+    punctaj = 0
+    for file in os.listdir(folder_name):
+        compile_cpp(os.path.join(folder_name, file))
+    for file in os.listdir(folder_name):
+        if file.endswith('.exe'):
+            for test in teste_data:
+                test_name = test['id']
+                test_input = test['input']
+                punctaj_test = test['punctaj']
+                test_expected_output= test['output']
+                if test_name == file[:-4]:
+                    print(f"Running test {test_name} with input {test_input} for file {file}")
+                    result = subprocess.run([os.path.join(folder_name, file)], input=test_input, text=True,capture_output=True)
+                    actual_output = result.stdout.strip()
+                    print(f"Expected output: {test_expected_output}")
+                    print(f"Actual output: {actual_output}")
+                    if actual_output == test_expected_output:
+                        print(f"Test {test_name} passed")
+                        punctaj += punctaj_test
+                    else:
+                        print(f"Test {test_name} failed")
+    print(f"Punctaj final: {punctaj}")
+
+
+
+
+
+
+
+#run_tests(get_tests('acummergi'), 'mario') 
+#print(get_tests('acummergi'))   
+
+
+
+#source_file = r'C:\Users\m3m0r\Projects\bacOS\mario\subiect1.cpp'
+#compile_cpp(source_file)
+
+compile_cpp(r'C:\Users\m3m0r\Projects\bacOS\mario\subiect1.cpp')
+print(run_tests(get_tests('acummergi'), 'mario'))
