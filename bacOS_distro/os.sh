f24f42ad9b13c1c9722511249c69b0961fcca295
nitumario <nitu.mario@gmail.com>
2024-07-12 21:05:52 +0300

    new file:   bacOS_distro/grub.cfg
    	modified:   bacOS_distro/kernel.cfg
    	modified:   bacOS_distro/os.sh

--- bacOS_distro/os.sh
+++ bacOS_distro/os.sh
@@ -1,35 +1,28 @@
-#installing the dependencies
-sudo apt install bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils
+#!/bin/bash
 
-#creating the workspace
-mkdir bacOS
 
-#downloading the kernel with its signature
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.8.tar.xz
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.8.tar.sign
+dependencies(){
+    sudo apt-get install \
+        fakeroot         \
+        build-essential  \
+        ncurses-dev xz-utils \
+        libssl-dev bc bison flex \
+        libelf-dev   \
+        xorriso \
+        git
+}
 
-unxz --keep linux-6.9.8.tar.xz
+workdir(){
+    mkdir -p $HOME/Desktop/bacOS
+    cd $HOME/Desktop/bacOS
+}
 
-#verifying the signature by importing the keys
-gpg2 --locate-keys torvalds@kernel.org gregkh@kernel.org
-gpg2 --verify linux-6.9.8.tar.sign
+get_kernel(){
+    wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.76.tar.gz
+    tar -xvf linux-5.10.76.tar.gz
+}
 
-#extracting the kernel
-tar xf linux-6.9.8.tar
-mv linux-6.9.8 bacOS
-cd bacOS/linux-6.9.8
-
-#using the config file from the host system(debian 12 in my case) i am going to use a custom config in the future for security reasons
-cp /boot/config-"$(uname -r)" .config
-make olddefconfig
-
-#since the kernel's signature is not verified, i am going to disable it
-./scripts/config --file .config --disable MODULE_SIG
-
-#setting the local version
-./scripts/config --file .config --set-str LOCALVERSION "-pratham"
-
-#building the kernel
-make -j$(nproc) 2>&1 | tee log
-sudo make modules_install -j$(nproc)
-sudo make install
+config_kernel(){
+    cd linux-5.10.76
+    wget https://raw.githubusercontent.com/nitumario/bacOS/main/bacOS_distro/kernel.cfg -O .config
+}
