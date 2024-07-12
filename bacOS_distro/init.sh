7737045549c10d6ed25c8f89d5a78f895c07c8fd
nitumario <nitu.mario@gmail.com>
2024-07-12 23:13:48 +0300

    modified:   bacOS_distro/init.sh

--- bacOS_distro/init.sh
+++ bacOS_distro/init.sh
@@ -10,9 +10,13 @@ mount -t devtmpfs none /dev
 mount -t proc none /proc
 mount -t sysfs none /sys
 
+# setup networking
+for NETDEV in /sys/class/net/* ; do
+  echo "Found network device ${NETDEV##*/}"
+  ip link set ${NETDEV##*/} up  
+  [ "${NETDEV##*/}" != "lo" ] && udhcpc -b -i ${NETDEV##*/} -s /etc/network.sh
+done
+
+
 # start BusyBox init
 exec /sbin/init
-
-
-
-
