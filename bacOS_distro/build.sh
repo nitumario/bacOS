55c08d5e0c0a0543ff47df53aa95e5bab8370090
nitumario <nitu.mario@gmail.com>
2024-07-28 22:18:57 +0300

    modified:   bacOS_distro/build.sh

--- bacOS_distro/build.sh
+++ bacOS_distro/build.sh
@@ -4,53 +4,53 @@
 set -e
 
 # Constants
-KERNEL_VER="linux-5.10.76.tar.xz"
-GLIBC_VER="glibc-2.38.tar.gz"
-BUSYBOX_VER="busybox-1.36.1.tar.bz2"
-PWD_DIR="$(pwd)"
-KERNEL_DIR="$PWD/bacos/$(echo $KERNEL_VER | sed 's/\.tar\.xz//')"
-GLIBC_DIR="$PWD/bacos/$(echo $GLIBC_VER | sed 's/\.tar\.gz//')"
-BUSYBOX_DIR="$PWD/bacos/$(echo $BUSYBOX_VER | sed 's/\.tar\.bz2//')"
-SYSROOT_DIR="$PWD/bacos/sysroot"
-bacos_DIR="$PWD/bacos"
-ISO_DIR="$PWD/bacos/iso"
+KERNEL_VERSION="linux-5.10.76.tar.xz"
+GLIBC_VERSION="glibc-2.38.tar.gz"
+BUSYBOX_VERSION="busybox-1.36.1.tar.bz2"
+CURRENT_DIR="$(pwd)"
+KERNEL_PATH="$CURRENT_DIR/bacos/$(basename $KERNEL_VERSION .tar.xz)"
+GLIBC_PATH="$CURRENT_DIR/bacos/$(basename $GLIBC_VERSION .tar.gz)"
+BUSYBOX_PATH="$CURRENT_DIR/bacos/$(basename $BUSYBOX_VERSION .tar.bz2)"
+SYSROOT_PATH="$CURRENT_DIR/bacos/sysroot"
+BACOS_PATH="$CURRENT_DIR/bacos"
+ISO_PATH="$CURRENT_DIR/bacos/iso"
 
 # Install dependencies
-get_deps() {
-  sudo apt-get install   \
-    fakeroot             \
-    build-essential      \
+install_dependencies() {
+  sudo apt-get install -y \
+    fakeroot \
+    build-essential \
     ncurses-dev xz-utils \
-    libssl-dev           \
-    bc                   \
-    flex                 \
-    libelf-dev           \
+    libssl-dev \
+    bc \
+    flex \
+    libelf-dev \
     bison
 }
 
 # Prepare build directory
-build_init() {
-  rm -rf $bacos_DIR
-  mkdir $bacos_DIR
+prepare_build() {
+  rm -rf $BACOS_PATH
+  mkdir $BACOS_PATH
 }
 
 # Build kernel
 build_kernel() {
-  cd $bacos_DIR
-  wget "https://cdn.kernel.org/pub/linux/kernel/v5.x/$KERNEL_VER"
-  tar -xvf $KERNEL_VER
-  cd $KERNEL_DIR
+  cd $BACOS_PATH
+  wget "https://cdn.kernel.org/pub/linux/kernel/v5.x/$KERNEL_VERSION"
+  tar -xvf $KERNEL_VERSION
+  cd $KERNEL_PATH
   make x86_64_defconfig -j $(nproc)
-  cp $PWD_DIR/config/kernel.cfg .config
+  cp $CURRENT_DIR/config/kernel.cfg .config
   make bzImage -j $(nproc)
 }
 
 # Build glibc
 build_glibc() {
-  cd $bacos_DIR
-  wget http://ftp.gnu.org/gnu/libc/$GLIBC_VER
-  tar -xvf $GLIBC_VER
-  cd $GLIBC_DIR
+  cd $BACOS_PATH
+  wget "http://ftp.gnu.org/gnu/libc/$GLIBC_VERSION"
+  tar -xvf $GLIBC_VERSION
+  cd $GLIBC_PATH
   mkdir build
   mkdir GLIBC
   cd build
@@ -61,9 +61,9 @@ build_glibc() {
 
 # Build sysroot
 build_sysroot() {
-  cd $bacos_DIR
+  cd $BACOS_PATH
   mkdir -p sysroot/usr
-  cp -r $GLIBC_DIR/GLIBC/* sysroot
+  cp -r $GLIBC_PATH/GLIBC/* sysroot
   cp -r GLIBC/include/* sysroot/include/
   cp -r GLIBC/lib/* sysroot/lib/
   rsync -a /usr/include sysroot
@@ -73,10 +73,10 @@ build_sysroot() {
 
 # Build busybox
 build_busybox() {
-  cd $bacos_DIR
+  cd $BACOS_PATH
   wget "https://busybox.net/downloads/busybox-1.36.1.tar.bz2"
-  tar -xvjf $BUSYBOX_VER
-  cd $BUSYBOX_DIR
+  tar -xvjf $BUSYBOX_VERSION
+  cd $BUSYBOX_PATH
   make defconfig
   sed -i "s|.*CONFIG_SYSROOT.*|CONFIG_SYSROOT=\"../sysroot\"|" .config
   sed -i "s|.*CONFIG_EXTRA_CFLAGS.*|CONFIG_EXTRA_CFLAGS=\"-L../sysroot/lib\"|" .config
@@ -85,52 +85,52 @@ build_busybox() {
 }
 
 # Install core
-build_rootfs() {
-  cd $PWD_DIR
-  wget https://github.com/nitumario/bacOS_distro/raw/main/root.zip
+install_rootfs() {
+  cd $CURRENT_DIR
+  wget "https://github.com/nitumario/bacOS_distro/raw/main/root.zip"
   unzip root.zip
-  cd $bacos_DIR
+  cd $BACOS_PATH
   rm -rf rootfs
   mkdir rootfs
-  cp -r $PWD_DIR/root/* $bacos_DIR/rootfs
-  cp -r $BUSYBOX_DIR/BUSYBOX/* $bacos_DIR/rootfs
-  cp $SYSROOT_DIR/lib/libm.so.6 $bacos_DIR/rootfs/lib/libm.so.6
-  cp $SYSROOT_DIR/lib/libc.so.6 $bacos_DIR/rootfs/lib/libc.so.6
-  cp $SYSROOT_DIR/lib/libresolv.so.2 $bacos_DIR/rootfs/lib/libresolv.so.2
-  cp $SYSROOT_DIR/lib/ld-linux-x86-64.so.2 $bacos_DIR/rootfs/lib/ld-linux-x86-64.so.2
-  cp $SYSROOT_DIR/bin/ldd $bacos_DIR/rootfs/bin/ldd
-  sed -i 's/bash/sh/' $bacos_DIR/rootfs/bin/ldd
-  cd $bacos_DIR/rootfs && ln -s lib lib64
-  rm $bacos_DIR/rootfs/linuxrc
+  cp -r $CURRENT_DIR/root/* $BACOS_PATH/rootfs
+  cp -r $BUSYBOX_PATH/BUSYBOX/* $BACOS_PATH/rootfs
+  cp $SYSROOT_PATH/lib/libm.so.6 $BACOS_PATH/rootfs/lib/libm.so.6
+  cp $SYSROOT_PATH/lib/libc.so.6 $BACOS_PATH/rootfs/lib/libc.so.6
+  cp $SYSROOT_PATH/lib/libresolv.so.2 $BACOS_PATH/rootfs/lib/libresolv.so.2
+  cp $SYSROOT_PATH/lib/ld-linux-x86-64.so.2 $BACOS_PATH/rootfs/lib/ld-linux-x86-64.so.2
+  cp $SYSROOT_PATH/bin/ldd $BACOS_PATH/rootfs/bin/ldd
+  sed -i 's/bash/sh/' $BACOS_PATH/rootfs/bin/ldd
+  cd $BACOS_PATH/rootfs && ln -s lib lib64
+  rm $BACOS_PATH/rootfs/linuxrc
   set +e
   strip -g \
-  $bacos_DIR/rootfs/bin/* \
-  $bacos_DIR/rootfs/sbin/* \
-  $bacos_DIR/rootfs/lib/* \
-  2>/dev/null
+    $BACOS_PATH/rootfs/bin/* \
+    $BACOS_PATH/rootfs/sbin/* \
+    $BACOS_PATH/rootfs/lib/* \
+    2>/dev/null
   set -e
-  rm -rf $PWD_DIR/root
-  rm $PWD_DIR/root.zip
+  rm -rf $CURRENT_DIR/root
+  rm $CURRENT_DIR/root.zip
 }
 
 # Create ISO file
-build_iso() {
-  cd $bacos_DIR
-  rm -rf $ISO_DIR
-  mkdir -p $ISO_DIR/boot/grub
-  cd $bacos_DIR/rootfs
-  find . | cpio -o -H newc | gzip > $ISO_DIR/boot/root.cpio.gz
-  cp $KERNEL_DIR/arch/x86/boot/bzImage $ISO_DIR/boot/bzImage
-  cp $PWD_DIR/config/grub.cfg $ISO_DIR/boot/grub/grub.cfg
-  grub-mkrescue -o $PWD_DIR/bacos-linux.iso $ISO_DIR
+create_iso() {
+  cd $BACOS_PATH
+  rm -rf $ISO_PATH
+  mkdir -p $ISO_PATH/boot/grub
+  cd $BACOS_PATH/rootfs
+  find . | cpio -o -H newc | gzip > $ISO_PATH/boot/root.cpio.gz
+  cp $KERNEL_PATH/arch/x86/boot/bzImage $ISO_PATH/boot/bzImage
+  cp $CURRENT_DIR/config/grub.cfg $ISO_PATH/boot/grub/grub.cfg
+  grub-mkrescue -o $CURRENT_DIR/bacos-linux.iso $ISO_PATH
 }
 
 # Main
-get_deps
-build_init
+install_dependencies
+prepare_build
 build_kernel
 build_glibc
 build_sysroot
 build_busybox
-build_rootfs
-build_iso
+install_rootfs
+create_iso
